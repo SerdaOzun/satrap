fa9d2490d78b00edbcf0f924068bbea92c8e00e7
Serda Özün <serda@mailbox.org>
2023-12-29 22:58:08 +0100

    Add option to copy the ssh command and server url

--- src/jvmMain/kotlin/screens/serverList/grid/ServerGridRow.kt
+++ src/jvmMain/kotlin/screens/serverList/grid/ServerGridRow.kt
@@ -1,13 +1,18 @@
 package screens.serverList.grid
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import domain.ServerComplete
 import domain.User
@@ -16,6 +21,7 @@ import ui.components.TerminalCombobox
 import util.runCommand
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ServerItem(
     serverComplete: ServerComplete,
@@ -28,9 +34,16 @@ fun ServerItem(
                 serverComplete.users.firstOrNull { it.userId == serverComplete.server.defaultUserId }
         )
     }
+    val clipboardManager = LocalClipboardManager.current
 
     Row(
-        modifier = Modifier.clickable { if (selectedId != serverComplete.server.serverId) onSelect(serverComplete.server.serverId) }
+        modifier = Modifier
+            .clickable { //Left mouse button click
+                if (selectedId != serverComplete.server.serverId) onSelect(serverComplete.server.serverId)
+            }
+            .onClick( //Right mouse button click
+                enabled = true, matcher = PointerMatcher.mouse(PointerButton.Secondary),
+                onClick = { clipboardManager.setText(AnnotatedString(serverComplete.server.serverUrl)) })
             .height(IntrinsicSize.Min)
             .border(width = 1.dp, color = MaterialTheme.colors.onPrimary),
         verticalAlignment = Alignment.CenterVertically
@@ -45,14 +58,20 @@ fun ServerItem(
             )
         }
 
+        //If the selectedUser is empty or its id is < 0 (meaning SSH Agent user) do not specify a username
+        val sshAction = if (selectedUser == null || selectedUser?.userId!! < 0) {
+            "ssh ${serverComplete.server.serverUrl}"
+        } else {
+            "ssh ${selectedUser?.username}@${serverComplete.server.serverUrl}"
+        }
+
         //Run Button
-        TableIconButton(modifier = Modifier.weight(ServerHeader.RUN.weight)) {
-            //If the selectedUser is empty or its id is < 0 (meaning SSH Agent user) do not specify a username
-            if (selectedUser == null || selectedUser?.userId!! < 0) {
-                runCommand("ssh ${serverComplete.server.serverUrl}")
-            } else {
-                runCommand("ssh ${selectedUser?.username}@${serverComplete.server.serverUrl}")
-            }
+        TableIconButton(icon = Icons.Filled.PlayArrow, modifier = Modifier.weight(ServerHeader.RUN.weight)) {
+            runCommand(sshAction)
+        }
+        //Copy run action to clipboard
+        TableImgButton(painter = painterResource("copy.png"), modifier = Modifier.weight(ServerHeader.COPY.weight)) {
+            clipboardManager.setText(AnnotatedString(sshAction))
         }
         //Servername/Serverurl
         TableCell(
--- src/jvmMain/kotlin/screens/serverList/util/ServerHeader.kt
+++ src/jvmMain/kotlin/screens/serverList/util/ServerHeader.kt
@@ -1,8 +1,9 @@
 package screens.serverList.util
 
 enum class ServerHeader(val label: String, val weight: Float, val headerWeight: Float = weight) {
-    RUN("", 0.05f ),
-    SERVER("Server", 0.25f, headerWeight = 0.3f),
+    RUN("", 0.04f ),
+    COPY("", weight = 0.04f),
+    SERVER("Server", 0.22f, headerWeight = 0.3f),
     USER("User", 0.2f),
     DESCRIPTION("Description", 0.5f);
 }
